42e1b8d46696c54a590bfce0fb396586c029bd30
Abdurrahman <capteinn@gmail.com>
2017-12-09 02:47:10 +0700

    menambahkan halaman berhasil

---
+++ application/views/berhasil.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Evently | E-Recruitment</title>
+
+		<!-- Bootstrap core CSS -->
+		 <!-- Bootstrap core CSS -->
+    	<link href="<?php echo base_url(); ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    	<link href="<?php echo base_url(); ?>assets/css/modern-business.css" rel="stylesheet">
+		<link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/lambangevent.ico" />
+
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/bootstrap/css/styles.css">
+	</head>
+
+	<body>
+
+		  <nav class="navbar fixed-top navbar-expand-lg bg-white fixed-top">
+      <div class="container">
+
+        
+        <!-- LOGO -->
+        <div class="logo" style="width: 250px"> 
+          <a href="<?php echo base_url(); ?>" title="Evently"><img src="<?php echo base_url(); ?>assets/images/evently.png" alt="Evently" /></a>
+        </div>
+
+        <div class="collapse" id='cssmenu'>
+			<ul>
+   				<li class='active'><a href='<?php echo base_url();?>'>Event</a></li>
+   				<li><a href='<?php echo base_url();?>about'>Tentang</a></li>
+			    <li><a href='<?php echo base_url();?>contact'>Contact</a></li>
+			</ul>
+		</div>
+
+
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Page Heading/Breadcrumbs -->
+      <h1 class="mt-4 mb-3 text-center">Selamat!
+        <small>Registrasi Berhasil</small>
+      </h1>
+
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="index.html">Beranda</a>
+        </li>
+        <li class="breadcrumb-item active">Berhasil</li>
+      </ol>
+
+      <div class="jumbotron">
+        <h1><i class="fa fa-check"></i></h1>
+        <h2>Pendaftaran Berhasil!</h2>
+        <p>Selamat Anda telah berhasil melakukan pendaftaran sebagai panitia event.</p>
+      </div>
+      <!-- /.jumbotron -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+		<footer class="py-3 bg-dark">
+			<div class="container">
+				<p class="m-0 text-center text-white">Copyright &copy; Proyek SI 2017</p>
+			</div>
+			<!-- /.container -->
+		</footer>
+
+		<!-- Bootstrap core JavaScript -->
+		<script src="<?php echo base_url();?>assets/vendor/jquery/jquery.min.js"></script>
+		<script src="<?php echo base_url();?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
